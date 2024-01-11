ead6306378104fc3bcd667d50f2d0829fac37c6b
Nikhil Gugwad <nikhilgugwad@gmail.com>
2024-01-12 01:19:33 +0530

    first commit

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import "./DescriptionBox.css";
+
+const DescriptionBox = () => {
+  return (
+    <div className="descriptionbox">
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad iure
+          fugiat ut sed, maiores facere deserunt et numquam culpa nisi similique
+          voluptatibus neque labore omnis perferendis vero nam tenetur,
+          praesentium non doloribus? Quam aliquid laborum adipisci quas
+          dignissimos culpa, cumque labore quidem provident quod facere,
+          temporibus officiis delectus nemo autem totam dicta! Fugit saepe
+          possimus, quaerat sequi a illum ut ad omnis culpa at expedita ea vero
+          delectus repellat odio adipisci numquam quisquam commodi perspiciatis
+          deserunt nobis doloremque officiis facilis ratione. Minus sit cumque
+          blanditiis atque reprehenderit. Iusto, rem perferendis saepe omnis
+          earum sapiente vitae distinctio iure, aspernatur deserunt odit.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+          nesciunt iusto at explicabo officiis temporibus possimus quaerat sequi
+          cum earum nulla repellendus sapiente obcaecati nam a labore
+          dignissimos modi expedita, id, voluptatem blanditiis ea eum eligendi.
+          Ea commodi vel labore.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default DescriptionBox;
